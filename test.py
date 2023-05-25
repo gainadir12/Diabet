1ceb874cb49d97d3400d4589da58829d78080c4a
gainadir12 <131377978+gainadir12@users.noreply.github.com>
2023-05-25 13:14:29 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 import pandas as pd
+# data = pd.read_csv('diabetes.csv')
+# print(data.head())
+# Загрузка данных
 data = pd.read_csv('diabetes.csv')
-print(data.head())
+# берем последние 10 строк из датафрейма
+last_10_rows = data.tail(10)
+
+# берем все строки, кроме последних 10
+other_rows = data.head(len(data) - 10)
+# сохраняем данные с последними 10 строками в файл
+last_10_rows.to_csv('last_10_rows.csv', index=False)
+
+# сохраняем остальные данные в другой файл
+other_rows.to_csv('other_rows.csv', index=False)
